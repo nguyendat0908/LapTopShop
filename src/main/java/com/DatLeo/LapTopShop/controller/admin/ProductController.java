b225a3041b5c6abcb86560f9c84899dd5cee3cc7
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-11-21 23:18:16 +0700

    Completed

--- src/main/java/com/DatLeo/LapTopShop/controller/admin/ProductController.java
+++ src/main/java/com/DatLeo/LapTopShop/controller/admin/ProductController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
